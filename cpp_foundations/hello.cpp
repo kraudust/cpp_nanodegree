68d1b1231f123f78f5c8692c841fae5c58540fa4
Dustan Kraus <dustan.kraus@gmail.com>
2023-10-11 10:29:31 -0700

    add chrono example

--- cpp_foundations/hello.cpp
+++ cpp_foundations/hello.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
 
 int main() {
+  std::chrono::steady_clock::time_point t0;
+  std::chrono::steady_clock::time_point t1;
+  t0 = std::chrono::steady_clock::now();
   std::cout << "Hello!\n";
+  sleep(5);
+  t1 = std::chrono::steady_clock::now();
+
+  std::chrono::duration<double> elapsed_seconds = t1 - t0;
+  std::chrono::duration<int64_t, std::micro> microseconds = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+  // std::chrono::microseconds().count();
+
+  std::cout << "seconds: " << elapsed_seconds.count() << std::endl;
+  std::cout << "microseconds: " << microseconds.count() << std::endl;
+
 }
 
